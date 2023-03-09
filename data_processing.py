5e816734327faa937d4bfd5a7480df86a5b2188e
Brian Farrell <farrelb6@tcd.ie>
2023-03-08 23:59:09 -0800

    changed data format

--- data_processing.py
+++ data_processing.py
@@ -17,23 +17,23 @@ def data_processing():
     path = kr_train + 'topical_chat.en-ko.en'
     data = dataframe_builder(data, path, 'English')
 
-    path = kr_train + 'telephony.en-ko.formal.annotated.ko'
+    path = kr_train + 'telephony.en-ko.formal.ko'
     data = dataframe_builder(data, path, 'Formal_Korean')
-    path = kr_train + 'topical_chat.en-ko.formal.annotated.ko'
+    path = kr_train + 'topical_chat.en-ko.formal.ko'
     data = dataframe_builder(data, path, 'Formal_Korean')
 
-    path = kr_train + 'telephony.en-ko.informal.annotated.ko'
+    path = kr_train + 'telephony.en-ko.informal.ko'
     data = dataframe_builder(data, path, 'Informal_Korean')
-    path = kr_train + 'topical_chat.en-ko.informal.annotated.ko'
+    path = kr_train + 'topical_chat.en-ko.informal.ko'
     data = dataframe_builder(data, path, 'Informal_Korean')
 
     data = pd.DataFrame(data)
-    data.to_csv('./data/train/en-ko/en-kr_combined_annotated')
+    data.to_csv('./data/train/en-ko/en-kr_combined')
     return 0
 
 
 def main():
-    # data_processing()
+    data_processing()
     data = pd.read_csv('data/train/en-ko/en-kr_combined_annotated')
     print(data['English'])
 
