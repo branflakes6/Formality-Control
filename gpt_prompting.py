bc61d02c4b6aafb9f0c56627fad43429c9d5c7f2
Brian Farrell <farrelb6@tcd.ie>
2023-03-10 18:44:36 -0800

    zero shot and nshot formality switching

--- gpt_prompting.py
+++ gpt_prompting.py
@@ -11,34 +11,33 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 LLM = OpenAI(temperature=0, model_name="text-curie-001")
 
 
-def get_model():
-    model = OpenAI(temperature=0, model_name="text-davinci-003")
-    return model
+def gold_labels(data, n_samples):
+    data = data[:n_samples]
+    english = []
+    kormal = []
+    informal = []
+    for x in data.iterrows():
+        english.append(x[1]['English'])
+        kormal.append(x[1]['Formal_Korean'])
+
+    f = open("formal_gold", "w", encoding='utf-8')
+    for x in kormal:
+        f.write(x)
+
+    f = open("eng_gold", "w", encoding='utf-8')
+    for x in english:
+        f.write(x)
+
+    f = open("informal_gold", "w", encoding='utf-8')
+    for x in informal:
+        f.write(x)
 
 
-def get_prompt(data, n, variable):
-    return data[0]
-
-
-def training_loop(data, option):
-    sentence = option
-    prompt = get_prompt(data, 1, sentence)
-    prompt = PromptTemplate(
-        input_variables=["sentence"],
-        template=prompt
-    )
-    chain = LLMChain(llm=LLM, prompt=prompt)
-    prediction = chain.run(sentence)
-    prediction = prediction[18:]
-    print("prediction", prediction)
-    return prediction
-
-
-def english_to_formal_prompt(data, n, variable):
+def english_to_formal_prompt_n_shot(data, n, query):
     data = data[:n]
     formal_kr = data['Formal_Korean']
     english = data['English']
-    prompt = ' '
+    prompt = ''
     for i, _ in enumerate(formal_kr):
         if english[i] != query:
             prompt = prompt + "English:" + english[i] + 'Formal Korean:' + formal_kr[i]
@@ -46,8 +45,7 @@ def english_to_formal_prompt(data, n, variable):
     return prompt
 
 
-# Prompt 'Formal Korean: formal, Informal Korean: informal'
-def formal_to_informal_prompt(data, n, query):
+def formal_to_informal_prompt_n_shot(data, n, query):
     data = data[:n]
     formal_kr = data['Formal_Korean']
     informal_kr = data['Informal_Korean']
@@ -59,7 +57,7 @@ def formal_to_informal_prompt(data, n, query):
     return prompt
 
 
-def prompt_test(prompt, query):
+def prompter(prompt, query):
     prompt = PromptTemplate(
         input_variables=['query'],
         template=prompt
@@ -68,73 +66,109 @@ def prompt_test(prompt, query):
     prediction = chain.run(query)
     return prediction
 
-def formal_to_informal(data):
-    data = data[:5]
-    preds = []
-    for x in tqdm(data.iterrows()):
-        query = x[1]['Formal_Korean']
-        prompt = formal_to_informal_prompt(data, 5, x)
-        query = 'Formal Korean:' + query + 'Informal Korean:'
-        print(query)
-        pred = prompt_test(prompt, query)
-        pred = pred + "\n"
-        preds.append(pred)
 
-    f = open("preds_informal", "w", encoding='utf-8')
-    for x in preds:
-        f.write(x)
-
-    data = pd.read_csv('data/train/en-ko/en-kr_combined_annotated')
-    data = data[:1]
-    inf = []
-    kor = []
-    for x in data.iterrows():
-        inf.append(x[1]['Informal_Korean'])
-        kor.append(x[1]['Formal_Korean'])
-
-    f = open("kor", "w", encoding='utf-8')
-    for x in kor:
-        f.write(x)
-
-    f = open("inf", "w", encoding='utf-8')
-    for x in inf:
-        f.write(x)
-
-
-def eng_to_formal(data):
+def eng_to_formal_n_shot(data, n_samples, n_shot):
+    data = data[:n_samples]
     preds = []
     for x in tqdm(data.iterrows()):
         query = x[1]['English']
-        prompt = english_to_formal_prompt(data, 5, x)
         query = 'English:' + query + 'Formal Korean:'
-        print(query)
-        print(prompt)
-        pred = prompt_test(prompt, query)
+        if n_shot > 0:
+            prompt = english_to_formal_prompt_n_shot(data, n_shot, x)
+        else:
+            prompt = '{query}'
+        pred = prompter(prompt, query)
         pred = pred + "\n"
         preds.append(pred)
 
     f = open("preds_formal)", "w", encoding='utf-8')
     for x in preds:
         f.write(x)
-
     data = pd.read_csv('data/train/en-ko/en-kr_combined_annotated')
-    data = data[:1]
-    eng = []
-    kor = []
-    for x in data.iterrows():
-        eng.append(x[1]['English'])
-        kor.append(x[1]['Formal_Korean'])
-
-    f = open("formal_kr", "w", encoding='utf-8')
-    for x in kor:
-        f.write(x)
-
-    f = open("eng_gold", "w", encoding='utf-8')
-    for x in eng:
-        f.write(x)
+    gold_labels(data, n_samples)
 
 
-data = pd.read_csv('data/train/en-ko/en-kr_combined')
-eng_to_formal(data)
-formal_to_informal(data)
+class FormalToInformal(object):
+    def __init__(self, data, formal_lang, informal_lang, prompt_func, file_path):
+        self.data = data
+        self.formal_lang = formal_lang
+        self.informal_lang = informal_lang
+        self.prompt_func = prompt_func
+        self.file_path = file_path
+
+    def zero_shot(self, zero_prompt, n_samples):
+        data = self.data[:n_samples]
+        preds = []
+        for x in tqdm(data.iterrows()):
+            query = x[1][self.formal_lang]
+            print("prompt", zero_prompt)
+            print("query", query)
+            # pred = prompter(zero_prompt, query)
+            # print("pred", pred)
+            # print("DONE")
+            # pred = pred + "\n"
+            # preds.append(pred)
+
+    def n_shot(self, n_samples):
+        data = self.data[:n_samples]
+        preds = []
+        for x in tqdm(data.iterrows()):
+            query = x[1][self.formal_lang]
+            prompt, query = self.prompt_func(data, x, query)
+            print("prompt", prompt)
+            print("query", query)
+            print("DONE")
+            pred = prompter(prompt, query)
+            print("pred", pred)
+            pred = pred + "\n"
+            preds.append(pred)
+
+        # f = open("preds_informal", "w", encoding='utf-8')
+        # for x in preds:
+        #     f.write(x)
+        # data = pd.read_csv('data/train/en-ko/en-kr_combined_annotated')
+        # gold_labels(data, n_samples)
+
+
+def zero_shot_korean_formal_to_informal():
+    data = pd.read_csv('data/train/en-ko/en-kr_combined')
+
+    def prompt_1(data, x, query):
+        return '', ''
+
+    korean_zero = FormalToInformal(data, 'Formal_Korean', 'Informal_Korean', prompt_1, '')
+    prompt = 'Can you convert this from Formal Korean to Informal Korean: {query}'
+    korean_zero.zero_shot(prompt, 3)
+    prompt = 'Can you give me the informal Korean of the following formal Korean sentence in Korean Hangul: {query}'
+    korean_zero.zero_shot(prompt, 1)
+    prompt = 'Can you give me the informal Korean of the following formal Korean sentence in Korean, please provide the output in the Korean language: {query}'
+    korean_zero.zero_shot(prompt, 1)
+
+
+def n_shot_korean_formal_to_informal():
+    data = pd.read_csv('data/train/en-ko/en-kr_combined')
+
+    def prompt_1(data, x, query):
+        prompt = formal_to_informal_prompt_n_shot(data, 5, x)
+        query = 'Formal Korean:' + query + 'Informal Korean:'
+        return prompt, query
+
+    kr_form_to_inform = FormalToInformal(data, 'Formal_Korean', 'Informal_Korean', prompt_1, '')
+    kr_form_to_inform.n_shot(2)
+
+    def prompt_2(data, x, query):
+        prompt = formal_to_informal_prompt_n_shot(data, 5, x)
+        query = 'Korean:' + query + 'Korean:'
+        return prompt, query
+
+    kr_form_to_inform.prompt_func = prompt_2
+    kr_form_to_inform.n_shot(2)
+
+    def prompt_3(data, x, query):
+        prompt = formal_to_informal_prompt_n_shot(data, 5, x)
+        query = 'Korean:' + query + 'Korean:'
+        return prompt, query
+
+    kr_form_to_inform.prompt_func = prompt_3
+    kr_form_to_inform.n_shot(2)
 
