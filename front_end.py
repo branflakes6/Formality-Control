b621a44d0d59a0e7d30b3b9b61babcadb7d2bab7
Brian Farrell <farrelb6@tcd.ie>
2023-03-23 17:49:14 -0700

    front_end

--- front_end.py
+++ front_end.py
@@ -1,29 +1,38 @@
 import streamlit as st
-import time
+import os
+from dotenv import load_dotenv
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
 
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+LLM = OpenAI(temperature = 0, model_name= "text-davinci-003")
 st.title('Korean Formality Control')
 
-def spoof(input):
+
+def run(query):
     with st.spinner('Translating'):
-        time.sleep(3)
-    formal_out = "그래서 아직도 너무 좋아하는 미문에 대한 애정을 말해요. 또한 애정을 이해해야 합니다."
+        prompt = PromptTemplate(
+                input_variables=["query"],
+                template="English: Yeah Did your mom know you were throwing the party?\n Formal Korean: 그, 어머님은 [F]님이[/F] 그 파티 연 거 [F]아세요[/F]?\n Informal Korean: 그, 어머님은 [F]네가[/F] 그 파티 연 거 [F]아셔[/F]?\n English: [F]내가[/F] [F]너[/F] 잘못되라고 하고 있다는 [F]거야[/F]?\n Formal Korean:[F]제가[/F] [F]님[/F] 잘못되라고 하고 있다는 [F]거예요[/F]?\n Informal Korean:[F]내가[/F] [F]너[/F] 잘못되라고 하고 있다는 [F]거야[/F]?\n English: {query} "
+            )
+        chain = LLMChain(llm=LLM, prompt=prompt)
+        formal = chain.run(query)
+
     with st.spinner('Converting'):
-        time.sleep(3)
-    time.sleep(3)
-    informal_out = "그래서 너무 좋아하는 미문에 대한 이유를 말해봐. 내가 왜 그렇게 좋아하는지 이해해야지."
-    return formal_out, informal_out
+        prompt = PromptTemplate(
+            input_variables=["formal"],
+            template="Formal Korean: 그, 어머님은 [F]님이[/F] 그 파티 연 거 [F]아세요[/F]?\n Informal Korean: 그, 어머님은 [F]네가[/F] 그 파티 연 거 [F]아셔[/F]?\n Formal Korean:[F]제가[/F] [F]님[/F] 잘못되라고 하고 있다는 [F]거예요[/F]?\n Informal Korean:[F]내가[/F] [F]너[/F] 잘못되라고 하고 있다는 [F]거야[/F]?\n Formal Korean: {sentence} "
+        )
+        chain = LLMChain(llm=LLM, prompt=prompt)
+        informal = chain.run(formal)
+    return formal, informal
 
-
-
-data = [
-    'Formal Korean: 그, 어머님은 [F]님이[/F] 그 파티 연 거 [F]아세요[/F]?\n Informal Korean: 그, 어머님은 [F]네가[/F] 그 파티 연 거 [F]아셔[/F]?\n Formal Korean:[F]제가[/F] [F]님[/F] 잘못되라고 하고 있다는 [F]거예요[/F]?\n Informal Korean:[F]내가[/F] [F]너[/F] 잘못되라고 하고 있다는 [F]거야[/F]?\n Formal Korean: {sentence}']
 
 
 input = st.text_input('Enter an English sentence to translate', '')
 if st.button('Translate'):
-    formal, informal = spoof(input)
+    formal, informal = run(input)
     st.write("Formal Korean", formal)
     st.write("Informal Korean", informal)
-
-
-
